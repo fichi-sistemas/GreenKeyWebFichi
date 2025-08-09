84acf32c06c0cab5bd3ece1d5e46868236d3b228
AndresSilOrt <andres.sil.ort@gmail.com>
2025-08-09 16:56:27 -0600

    PDF included to webpage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-document.getElementById('btnSaludo').addEventListener('click', function() {
-    alert('¡Hola! Gracias por visitar la página de la empresa.');
+// Botón para visitar la página oficial
+document.getElementById('btnVerMas').addEventListener('click', function() {
+    window.location.href = 'https://greenkeymexico.org/';
+});
+
+// Botón para abrir el PDF en una nueva pestaña
+document.getElementById('btnVerPDF').addEventListener('click', function() {
+    window.open('docs/greenkey-info.pdf', '_blank');
 });
 
